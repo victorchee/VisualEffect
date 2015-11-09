fb10eeaadac570e89bada41c838bb4aa92239133
Victor Chee <victor.qhj@gmail.com>
2015-11-09 14:12:14 +0800

    update visual effect

--- VisualEffect/VisualEffect/ViewController.swift
+++ VisualEffect/VisualEffect/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Bottom --> Top
+        // BackgroundView -> BlurEffectView -> VibrancyEffectView -> LabelView
     }
 
     override func didReceiveMemoryWarning() {
